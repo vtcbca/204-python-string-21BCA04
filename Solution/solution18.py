87c9318cf841685661b7b1e79261d559dcecd8b5
KAMLESH KUMAWAT <96828566+21BCA04@users.noreply.github.com>
2022-03-24 11:09:48 +0530

    solution 18 uploaded

--- Solution/solution18.py
+++ Solution/solution18.py
@@ -1,14 +1,13 @@
 #WAS to enter 5 string in a list and check and print string those lenth has even no of char.
 #NOTE.:Do this UDF.... 
-        #This is For only 1 string
 def strlen(s):
     count=0
     for i in s:
         count+=1
     return(count)
-str=input("string:")
-ans=strlen(str)
-print("length of {} is {}".format(str,ans))
-if(ans%2==0):
-    print(str)
+for i in range(5):
+        str=input("string:")
+        ans=strlen(str)
+        if(ans%2==0):
+                print(str)
 
